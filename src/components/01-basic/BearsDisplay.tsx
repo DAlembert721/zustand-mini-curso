61097f9c40beefab2aeabc945fb9bd0fd8e1f1f8
Dalembert Monzón Urrutia <dalembert.dmu@gmail.com>
2023-10-27 03:46:18 -0500

    Add zustand to bears page

---
+++ src/components/01-basic/BearsDisplay.tsx
@@ -0,0 +1,23 @@
+import { useShallow } from "zustand/react/shallow";
+import { WhiteCard } from "..";
+import { useBearStore } from "../../stores";
+
+const BearsDisplay = () => {
+  const bears = useBearStore(useShallow((state) => state.bears));
+  const doNothing = useBearStore((state) => state.doNothing);
+  const clearBears = useBearStore((state) => state.clearBears);
+  const addBear = useBearStore((state) => state.addBear);
+  return (
+    <WhiteCard>
+      <h1>Osos</h1>
+      <button onClick={doNothing}>Do Nothing</button>
+      <button className="mt-2" onClick={addBear}>Agregar oso</button>
+      <button className="mt-2" onClick={clearBears}>Borrar Oso</button>
+      <pre>
+        {JSON.stringify(bears, null, 2)}
+      </pre>
+    </WhiteCard>
+  );
+};
+
+export default BearsDisplay;
